1105e6d79c17cf4344393c573de5a6e6d839e46d
VladisP <vlad19041999@yandex.ru>
2019-05-10 20:43:06 +0300

    Авторизация и регистрация значительно улучшены.

---
+++ app/src/main/java/com/example/yetanotherfitapp/activities/EntryActivity.java
@@ -0,0 +1,98 @@
+package com.example.yetanotherfitapp.activities;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.yetanotherfitapp.R;
+import com.example.yetanotherfitapp.fragments.EntryFragment;
+import com.example.yetanotherfitapp.fragments.RegistrationFragment;
+import com.example.yetanotherfitapp.fragments.SignInFragment;
+import com.google.firebase.auth.FirebaseAuth;
+
+//TODO: Add design
+//TODO: Add mail check
+//TODO: Add multithreading
+//TODO: Добавить обработку профиля пользователя при успешном входе
+//TODO: Обработка смены конфигурации!
+//TODO: Clean Architecture
+
+public class EntryActivity extends AppCompatActivity implements EntryFragment.OnAuthStateChangeListener {
+
+    private enum mAuthState {
+        ENTRY,
+        SIGNIN,
+        REG
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            success("Добро пожаловать!");
+        } else {
+            showAuthFragment(mAuthState.ENTRY);
+        }
+    }
+
+    private void showAuthFragment(mAuthState state) {
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment currentFragment = fragmentManager.findFragmentById(R.id.fragment_login_container);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        switch (state) {
+            case ENTRY:
+                fragment = new EntryFragment();
+                break;
+            case REG:
+                fragment = new RegistrationFragment();
+                break;
+            case SIGNIN:
+                fragment = new SignInFragment();
+                break;
+        }
+
+        if (currentFragment == null) {
+            transaction.add(R.id.fragment_login_container, fragment);
+        } else {
+            transaction.replace(R.id.fragment_login_container, fragment);
+        }
+
+        if (state != mAuthState.ENTRY) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+    @Override
+    public void needReg() {
+        showAuthFragment(mAuthState.REG);
+    }
+
+    @Override
+    public void needSignIn() {
+        showAuthFragment(mAuthState.SIGNIN);
+    }
+
+    @Override
+    public void success(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(EntryActivity.this, ExerciseListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void fail(String errorMsg) {
+        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
+    }
+
+}
---
+++ app/src/main/java/com/example/yetanotherfitapp/fragments/SignInFragment.java
@@ -0,0 +1,109 @@
+package com.example.yetanotherfitapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.example.yetanotherfitapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private ProgressBar mProgressBar;
+    private EntryFragment.OnAuthStateChangeListener mOnAuthStateChangeListener;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mOnAuthStateChangeListener = (EntryFragment.OnAuthStateChangeListener) getActivity();
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        mEmailField = view.findViewById(R.id.signInEmail);
+        mPasswordField = view.findViewById(R.id.signInPassword);
+        mProgressBar = view.findViewById(R.id.signInProgress);
+
+        view.findViewById(R.id.signInBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            }
+        });
+
+        return view;
+    }
+
+    private void signIn(String email, String password) {
+        showProgress();
+
+        if (!validateForm()) {
+            hideProgress();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        hideProgress();
+
+                        if (task.isSuccessful()) {
+                            mOnAuthStateChangeListener.success("Добро пожаловать!");
+                        } else {
+                            mOnAuthStateChangeListener.fail(task.getException().getMessage());
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailField.setError("Поле заполнено некорректно");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Поле заполнено некорректно");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void showProgress() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void hideProgress() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+}
